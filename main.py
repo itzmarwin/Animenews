331adb221da4f9de3f58992b57381d6b4cb534fa
itzmarwin <itzm4rw1n@gmail.com>
2025-07-25 21:53:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from media_handler import download_image, download_youtube_video
 # Configure logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    level=logging.DEBUG
 )
 logger = logging.getLogger(__name__)
 
